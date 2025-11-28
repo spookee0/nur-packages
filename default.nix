5c24b6aa945a00cfdc64099f12d92374ba9b2377
spookee0 <spookee@disroot.org>
2025-11-28 10:18:54 +0300

    add openvsp

--- default.nix
+++ default.nix
@@ -5,7 +5,19 @@
 # Having pkgs default to <nixpkgs> is fine though, and it lets you use short
 # commands such as:
 #     nix-build -A mypackage
-{pkgs ? import <nixpkgs> {}}: {
+{pkgs ? import <nixpkgs> {}}: let
+  # Pin nixpkgs to a revision that has cmake 3.24.3
+  # This commit is from around the time when cmake 3.24.3 was current
+  # (before it was updated to 3.25.2 in nixpkgs) [[1]]
+  pkgsWithCmake3_27 =
+    import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/cc9458e2b9a56c085a34e3b648300e46a87e94a3.tar.gz";
+      sha256 = "0zflafb2srlpjrrw5qi2xhk1d1nx7akvaj3qdjcxqpxvh48xgskx";
+    }) {
+      system = pkgs.stdenv.hostPlatform.system;
+    };
+  cmake-3_27 = pkgsWithCmake3_27.cmake;
+in {
   # The `lib`, `modules`, and `overlays` names are special
   lib = import ./lib {inherit pkgs;}; # functions
   modules = import ./modules; # NixOS modules
@@ -13,4 +25,7 @@
 
   at32-work-bench = pkgs.libsForQt5.callPackage ./pkgs/at32-work-bench {};
   elf-size-analyze = pkgs.python3Packages.callPackage ./pkgs/elf-size-analyze {};
+  openvsp = pkgs.callPackage ./pkgs/openvsp {
+    inherit cmake-3_27;
+  };
 }
---
+++ pkgs/openvsp/default.nix
@@ -0,0 +1,114 @@
+{
+  stdenv,
+  fetchFromGitHub,
+  git,
+  lib,
+  unzip,
+  # installed by developer
+  cmake-3_27,
+  swig,
+  python3,
+  doxygen,
+  graphviz,
+  # bundled, but system libs are ok
+  # angelscript, # needs additional packages
+  clipper2,
+  cminpack,
+  cpptest,
+  eigen,
+  fltk14,
+  glew,
+  glm,
+  # libxml2, # does not compile for some reason
+}:
+stdenv.mkDerivation rec {
+  pname = "openvsp";
+  version = "3.46.0";
+
+  src = fetchFromGitHub {
+    owner = "OpenVSP";
+    repo = "OpenVSP";
+    rev = "OpenVSP_${version}";
+    hash = "sha256-zSjWSM5+2tYM5uHssR/ECtJcnwkF/rxsj7obmRpIyu4=";
+  };
+
+  nativeBuildInputs = [
+    cmake-3_27
+    python3
+    git
+    cpptest
+    unzip
+  ];
+
+  # swig & doxygen are not included as the build would fail since it tries to call
+  # "swig -doxygen" which fails Make as this is not a valid command.
+  # Seems like an upstream problem.
+  buildInputs = [
+    swig
+    doxygen
+    graphviz
+    # angelscript
+    clipper2
+    cminpack
+    eigen
+    fltk14
+    glew
+    glm
+    # libxml2
+  ];
+
+  configurePhase = ''
+    mkdir -p build buildlibs
+    pushd buildlibs
+    cmake \
+      -DVSP_USE_SYSTEM_ANGELSCRIPT=false \
+      -DVSP_USE_SYSTEM_CLIPPER2=true \
+      -DVSP_USE_SYSTEM_CMINPACK=true \
+      -DVSP_USE_SYSTEM_CODEELI=false \
+      -DVSP_USE_SYSTEM_CPPTEST=true \
+      -DVSP_USE_SYSTEM_DELABELLA=false \
+      -DVSP_USE_SYSTEM_EIGEN=true \
+      -DVSP_USE_SYSTEM_EXPRPARSE=false \
+      -DVSP_USE_SYSTEM_FLTK=true \
+      -DVSP_USE_SYSTEM_GLEW=true \
+      -DVSP_USE_SYSTEM_GLM=true \
+      -DVSP_USE_SYSTEM_LIBIGES=false \
+      -DVSP_USE_SYSTEM_LIBXML2=false \
+      -DVSP_USE_SYSTEM_OPENABF=false \
+      -DVSP_USE_SYSTEM_PINOCCHIO=false \
+      -DVSP_USE_SYSTEM_STEPCODE=false \
+      -DVSP_USE_SYSTEM_TRIANGLE=false \
+      $src/Libraries \
+      -DCMAKE_BUILD_TYPE=Release \
+
+    make -j$cores
+    popd
+
+    pushd build
+    cmake $src/src/ -DVSP_LIBRARY_PATH=$PWD/../buildlibs -DCMAKE_BUILD_TYPE=Release
+    popd
+  '';
+
+  buildPhase = ''
+    pushd build
+    make -j$cores
+    make package
+    popd
+  '';
+
+  installPhase = ''
+    pushd build
+    mkdir -p $out/bin
+    unzip "OpenVSP-${version}-Linux.zip"
+    cp -r "OpenVSP-${version}-Linux"/* $out/bin
+    popd
+  '';
+
+  meta = {
+    description = "Parametric aircraft geometry tool";
+    homepage = "https://openvsp.org/";
+    license = lib.licenses.nasa13;
+    maintainers = with lib.maintainers; [kekschen];
+    mainProgram = "vsp";
+  };
+}
